80be467a05b8135d47bf957fe17e6ddcb245d48a
Dwi Bonggo <dwibonggo25@gmail.com>
2020-06-12 10:16:14 +0700

    First upload

---
+++ SimpleAppsDicoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleAppsDicoding
+//
+//  Created by Dwi Bonggo Pribadi on 6/9/20.
+//  Copyright © 2020 Dwi Bonggo Pribadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
